d4c64f804e7fae501d295da84f30a9dbbd817919
Khushbu <sheikh.khushbu13@gmail.com>
2020-09-07 15:58:17 +0530

    1st commit

---
+++ js/landing.js
@@ -0,0 +1,47 @@
+function landingvid() {
+
+    console.log("getting to func");
+    var vid = document.createElement("video");
+    vid.src = "assets/mp4/landingvid.mp4"
+    viddiv = document.getElementById("videodiv");
+    vid.autoplay = true;
+    viddiv.appendChild(vid);
+    vid.addEventListener('ended', function() {
+        window.location = 'mainpage.html';
+    });
+}
+
+function characvid() {
+    window.location.reload();
+}
+
+
+//vid.onended = function() {
+
+//        window.location = 'landingpage.html';
+//  }
+//vid.addEventListener('ended', function() {
+//    alert('video is ended');
+//   window.location.pathname = 'landingpage.html';
+//  })
+//onload="vidfunc"
+
+//console.log("getting tp the end");
+//window.history.go(-2);
+//window.location = "landingpage.html";
+//window.location.pathname = "landingpage.html"
+// console.log("endd");
+
+
+//}
+
+
+//}
+//var img = new Image();
+//div = document.getElementById('x');
+//img.onload = function() {
+//   div.appendChild(img);
+//};
+
+// img.src = "tinytan/landimg.png";
+//console.log(img);
